66e9225630c9fad4403952b96ed3abfbbf013543
laurenmetz17 <103585965+laurenmetz17@users.noreply.github.com>
2023-07-10 00:32:24 -0400

    commit tree 8bcfe66279c6b176a6d8991a62f49da4df5bb015

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,34 @@
 require_relative './stack'
 
+def balancing_parentheses(str)
+    stack = Stack.new
+    @count = 0
+    @length = str.length
+    str.split('') do |char|
+        if char ==")" 
+            if stack.size == 0
+                stack.push(char)
+            else
+                if stack.peek ==")"
+                    stack.push(char)
+                elsif stack.peek == "("
+                    stack.pop
+                end
+            end
+        elsif char == "("
+            if stack.size == 0
+                stack.push(char)
+            else
+                if stack.peek == "("
+                    stack.push(char)
+                elsif stack.peek == ")"
+                    stack.push(char)
+                end
+            end
+        end
+    end
+
+    return (stack.size)
+ 
+end
 # your code here
